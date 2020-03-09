1ae0caf8dceabae50a4f3d6683170f9716c5718f
Kelly Chuang <kchuang95@yahoo.com>
2020-03-09 16:55:30 -0500

    Initial Commit

---
+++ ChuangK Final Project- Cook Starter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChuangK Final Project- Cook Starter
+//
+//  Created by Kelly Chuang on 3/9/20.
+//  Copyright © 2020 Kelly Chuang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChuangK Final Project- Cook Starter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ChuangK Final Project- Cook Starter
+//
+//  Created by Kelly Chuang on 3/9/20.
+//  Copyright © 2020 Kelly Chuang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
